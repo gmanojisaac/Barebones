914c53c8cd75c771e7d5c95542630d60a7e22582
merlinpratheesh <merlinpratheesh@gmail.com>
2020-12-30 17:03:27 +0530

    workingtruefalse

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-
-
+import { BehaviorSubject, Subscription, Observable } from 'rxjs';
+import { UserdataService } from './service/userdata.service';
+import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +11,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'goldenNoStrict';
+  myauth;
+  subjectauth = new BehaviorSubject(undefined);
+  getObservableauthStateSub: Subscription = new Subscription;
+  getObservableauthState = (authdetails: Observable<firebase.User>) => {
+    if (this.getObservableauthStateSub !== undefined) {
+      this.getObservableauthStateSub.unsubscribe();
+    }
+    this.getObservableauthStateSub = authdetails.subscribe((val: any) => {
+      console.log('val-53', val);
+      if(val === null){//logoff case
+        this.subjectauth.next(false);
+        console.log(val);
+
+      }else{//login case
+        console.log(val);
+        this.subjectauth.next(true);
+      }
+      
+    });
+    return this.subjectauth;
+  };
   
 
-  constructor( ){
-
+  constructor( public afAuth: AngularFireAuth,    public developmentservice: UserdataService
+    ){
+    this.myauth = this.getObservableauthState(this.afAuth.authState);
   }
 }
--- src/app/service/userdata.service.ts
+++ src/app/service/userdata.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-
+import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from 'firebase/app';
 
 @Injectable({
   providedIn: 'root'
@@ -7,7 +8,23 @@ import { Injectable } from '@angular/core';
 export class UserdataService {
 
 
-  constructor() {
+  constructor(public auth: AngularFireAuth) {
 
    }
+
+   login() {
+    return this.auth.signInWithRedirect( new (firebase.auth as any).GoogleAuthProvider()).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      if (errorCode === 'auth/popup-closed-by-user' || errorCode === 'auth/network-request-failed'){
+        
+        //alert('Check Internet Connection');
+        location.reload();
+      }
+    });
+  }
+  async logout() {
+    console.log('72-reached');
+    return await this.auth.signOut();
+  }
 }
