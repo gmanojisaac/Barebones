424f578ad7782b56550a6ed1e6690f8d83a8085f
merlinpratheesh <merlinpratheesh@gmail.com>
2021-01-02 10:57:31 +0530

    worked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -42,183 +42,12 @@ export class AppComponent {
   };
   AfterOnlineCheckAuth=undefined;
 
-  myProfileInfo = undefined;
-  getProfileInfoBehaviourSub = new BehaviorSubject(undefined);
-  getProfileInfoSubscription: Subscription;
-  getProfileInfo = (ProfileInfoDoc: AngularFirestoreDocument<myusrinfo>) => {
-    if (this.getProfileInfoSubscription !== undefined) {
-      this.getProfileInfoSubscription.unsubscribe();
-    }
-    this.getProfileInfoSubscription = ProfileInfoDoc.valueChanges().subscribe(async (val: myusrinfo) => {
-      
-      if (val === undefined) {
-        this.getProfileInfoBehaviourSub.next(undefined);
-      }else{
-        console.log('53',val);
-        this.getProfileInfoBehaviourSub.next(val);
-      }
-    });
-    return this.getProfileInfoBehaviourSub;
-  };
 
-  Sections;
-  getSectionsSubscription: Subscription;
-  getSectionsBehaviourSub = new BehaviorSubject(undefined);
-  getSections = (MainAndSubSectionkeys: AngularFirestoreDocument<MainSectionGroup>) => {
-    if (this.getSectionsSubscription !== undefined) {
-      this.getSectionsSubscription.unsubscribe();
-    }
-    this.getSectionsSubscription = MainAndSubSectionkeys.valueChanges().subscribe((val: any) => {
-      if (val === undefined) {
-        this.getSectionsBehaviourSub.next(undefined);
-      } else {
-        if (!Object.keys(val.MainSection).length === true) {
-          this.getSectionsBehaviourSub.next(undefined);
-        } else {
-          if (val.MainSection !== undefined) {
-            this.getSectionsBehaviourSub.next(val.MainSection);
-          }
-        }
-      }
-    });
-    return this.getSectionsBehaviourSub;
-  };
-
-  publicList;
-  localpublicList: string[] = [];
-  getPublicListSubscription: Subscription;
-  getPublicListBehaviourSub = new BehaviorSubject(undefined);
-  getPublicList = (publicProjects: AngularFirestoreDocument<any>) => {
-    if (this.getPublicListSubscription !== undefined) {
-      this.getPublicListSubscription.unsubscribe();
-    }
-    this.getPublicListSubscription = publicProjects.valueChanges().subscribe((val: any) => {
-      if (val === undefined) {
-        this.getPublicListBehaviourSub.next(undefined);
-      } else {
-        if (val.public === undefined || !Object.keys(val.public).length === true) {
-          this.getPublicListBehaviourSub.next(undefined);
-        } else {
-          if (val.public !== undefined) {
-            //console.log('val.public',val.public);
-            this.localpublicList = val.public;
-            this.getPublicListBehaviourSub.next(val.public);
-          }
-        }
-      }
-    });
-    return this.getPublicListBehaviourSub;
-  };
-
-  privateList;
-  localprivateList: string[] = [];
-  getPrivateListSubscription: Subscription;
-  getPrivateListBehaviourSub = new BehaviorSubject(undefined);
-  getPrivateList = (privateProjects: AngularFirestoreDocument<any>) => {
-    if (this.getPrivateListSubscription !== undefined) {
-      this.getPrivateListSubscription.unsubscribe();
-    }
-    this.getPrivateListSubscription = privateProjects.valueChanges().subscribe((val: any) => {
-      if (val === undefined) {
-        this.getPrivateListBehaviourSub.next(undefined);
-      } else {
-        if (val.ownerRecord === undefined || !Object.keys(val.ownerRecord).length === true) {
-          //3 types of response
-          //1. no doc uid in projectList Collection-> returns undefined
-          //2. doc is there with testcase field and no ownerRecord field-> val.ownerRecord returns undefined
-          //3.doc/ownerRecord field is there with empty array of private projects-> objectCheck Length will be false
-
-          this.getPrivateListBehaviourSub.next(undefined);
-        } else {
-          if (val.ownerRecord !== undefined) {
-            this.localprivateList = val.ownerRecord;
-            this.getPrivateListBehaviourSub.next(val.ownerRecord);
-          }
-        }
-      }
-    });
-    return this.getPrivateListBehaviourSub;
-  };
-
-  SectionTc;
-  getTestcasesSubscription: Subscription;
-  getTestcasesBehaviourSub = new BehaviorSubject(undefined);
-  getTestcases = (TestcaseList: AngularFirestoreDocument<TestcaseInfo>) => {
-    if (this.getTestcasesSubscription !== undefined) {
-      this.getTestcasesSubscription.unsubscribe();
-    }
-    this.getTestcasesSubscription = TestcaseList.valueChanges().subscribe((val: any) => {
-      let arrayeverse = val;
-      if (val === undefined) {
-        arrayeverse = undefined;
-      } else {
-        if (!Object.keys(val.testcase).length === true) {
-          arrayeverse = undefined;
-        } else {
-          if (val.testcase !== undefined) {
-            arrayeverse = (val.testcase);
-            this.myprojectVariables.testcaseslength = arrayeverse.length;
-          } else {
-            arrayeverse = undefined;
-          }
-        }
-      }
-      this.getTestcasesBehaviourSub.next(arrayeverse);
-    });
-
-    return this.getTestcasesBehaviourSub;
-  };
-
-  myusrinfo: myusrinfo= {
-    MembershipEnd:undefined,
-    MembershipType:undefined,
-    projectLocation:undefined,
-    projectName:undefined,
-    projectOwner:undefined
-  };
-
-  myuserProfile: userProfile = {
-    userAuthenObj: null,//Receive User obj after login success
-    myusrinfoFromDb:null,
-    keysReadFromDb:undefined,
-    mainsubsectionKeys:undefined,
-    subSectionKeys:undefined,
-    savedMainSectionKey:undefined,
-    savesubSectionKeys:undefined,
-    savedisabledval:undefined
-   };
-   myprojectFlags: projectFlags= {    
-    showPaymentpage:false
-  };
-  myprojectVariables: projectVariables = {
-    initialMainSection:undefined,
-    testcaseslength:0,
-    viewSelectedTestcase: undefined
-  };
-  myprojectControls: projectControls = {
-    subsectionkeysControl: new FormControl(null, Validators.required),
-    testcaseInfoControl: new FormControl(),
-    createTestcaseControl: new FormControl(),
-    publicprojectControl: new FormControl(null, Validators.required),
-    ownPublicprojectControl: new FormControl(null, Validators.required),
-    firstMainSecControl: new FormControl(null, Validators.required),
-    editMainsectionGroup: this.fb.group({
-      editMainsectionControl: [{ value: '' }, Validators.required]
-    }),
-    visibilityMainsectionGroup: this.fb.group({
-      editVisibilityControl: [{ value: false, disabled: false }, Validators.required]
-    }),
-    editSubsectionGroup: this.fb.group({
-      editSubsectionControl: [{ value: '' }]
-
-    })
-  };
 
   constructor( 
     public afAuth: AngularFireAuth,    
     public developmentservice: UserdataService,
     private db: AngularFirestore,
-    public fb: FormBuilder
     ){     
 
 
@@ -231,120 +60,23 @@ export class AppComponent {
         return this.myauth.pipe(
           filter(authstat => authstat !== undefined),//logincase track offline,logoutcase take else branch
           filter(authstat => authstat !== null),//only logged in case
-          switchMap((afterauth: any) => {
+          map((afterauth:firebase.User) => {
             console.log('53',afterauth);
 
-            this.myuserProfile.userAuthenObj=afterauth;
-            return this.getProfileInfo(this.db.doc('myProfile/' + afterauth.uid)).pipe(
-              map((profileval: any)=>{ 
-                console.log('250',profileval)   ;            
-                if(profileval === undefined){
-                  const nextMonth: Date = new Date();
-                  nextMonth.setMonth(nextMonth.getMonth() + 1);
-                  const newItem:myusrinfo = {
-                    MembershipEnd: nextMonth,
-                    MembershipType: 'Demo',
-                    projectLocation: '/projectList/DemoProjectKey',
-                    projectOwner: false,
-                    projectName: 'Demo'
-                  };
-                  this.myuserProfile.myusrinfoFromDb= newItem;
-                } else {
-                  this.myuserProfile.myusrinfoFromDb=profileval;
-                  if (new Date(this.myuserProfile.myusrinfoFromDb.MembershipEnd).valueOf() < new Date().valueOf()) {
-                    if (this.myuserProfile.myusrinfoFromDb.MembershipType === 'Demo') {//expired
-                      this.myuserProfile.myusrinfoFromDb.projectOwner = false;//cannot add tc
-                      this.myuserProfile.myusrinfoFromDb.projectName = 'Demo';
-                      this.myuserProfile.myusrinfoFromDb.projectLocation = '/projectList/DemoProjectKey';
-                      this.myuserProfile.myusrinfoFromDb.MembershipType = 'Demo';
-                      this.myuserProfile.myusrinfoFromDb.MembershipEnd = new Date(this.myuserProfile.myusrinfoFromDb.MembershipEnd);
-                      this.myprojectFlags.showPaymentpage = true;// show only payments Page
-                    } else {//expired member
-                      const nextMonth: Date = new Date();
-                      nextMonth.setMonth(nextMonth.getMonth() + 1);
-                      const newItem = {
-                        MembershipEnd: nextMonth.toDateString(),
-                        MembershipType: 'Demo',
-                        projectLocation: '/projectList/DemoProjectKey',
-                        projectOwner: true,
-                        projectName: 'Demo'
-                      };
-                      this.db.doc<any>('myProfile/' + this.myuserProfile.userAuthenObj.uid).set(newItem);
-                      this.myuserProfile.myusrinfoFromDb.projectOwner = true;
-                      this.myuserProfile.myusrinfoFromDb.projectName = 'Demo';
-                      this.myuserProfile.myusrinfoFromDb.projectLocation = '/projectList/DemoProjectKey';
-                      this.myuserProfile.myusrinfoFromDb.MembershipType = 'Demo';
-                      this.myuserProfile.myusrinfoFromDb.MembershipEnd = new Date(nextMonth.toDateString());
-                      this.myprojectFlags.showPaymentpage = false;
-                    }
-                  } else {//start normal
-                    this.myprojectFlags.showPaymentpage = false;
-                  }//end normal
-                  this.Sections= this.getSections(this.db.doc(this.myuserProfile.myusrinfoFromDb.projectLocation));
-                  this.publicList= this.getPublicList(this.db.doc(('/projectList/publicProjects')));
-                  this.privateList = this.getPrivateList(this.db.doc(('/projectList/' + this.myuserProfile.userAuthenObj.uid)));                  
-                  this.SectionTc = this.myprojectControls.subsectionkeysControl.valueChanges
-                  .pipe(startWith({ value: '', groupValue: '' }),
-                    map((selection: any) => {
-                      if (!selection || selection.groupValue === '') {
-                        this.myprojectVariables.initialMainSection = 'SubSection';
-                      } else {
-                        this.myprojectVariables.initialMainSection = selection.groupValue;
-                        if ( this.myuserProfile.myusrinfoFromDb.projectName === 'Demo') {
-                          return this.getTestcases(this.db.doc( 'projectList/' + this.myuserProfile.userAuthenObj.uid));
-                        } else {
-                          return this.getTestcases(this.db.doc('/' + this.myuserProfile.myusrinfoFromDb.projectName + '/' + selection.groupValue + '/items/' + selection.value));
-                        }            
-                      }
-                    })
-                  );
-                }
-                return onlineval;
-              }));
+            return afterauth;
 
-            /*const nextMonth: Date = new Date();
-            nextMonth.setMonth(nextMonth.getMonth() + 1);
-            const newItem:myusrinfo = {
-              MembershipEnd: nextMonth,
-              MembershipType: 'Demo',
-              projectLocation: '/projectList/DemoProjectKey',
-              projectOwner: false,
-              projectName: 'Demo'
-            };
-            this.myuserProfile.myusrinfoFromDb=newItem;
-            this.getProfileInfoSubscription?.unsubscribe();
-            this.getSectionsSubscription?.unsubscribe();
-            this.getPublicListSubscription?.unsubscribe();
-            this.getPrivateListSubscription?.unsubscribe();
-            return false;*/
-   
           })
         )
+        return onlineval;
+
       })
-    );
+      
+    )
   }
-  changelist(){
-    const some=[
-      "some",
-      "fwsd"
-    ];
-    this.getPublicListBehaviourSub.next(some);
-  }
+
+
   componentLogOff(){
-    const nextMonth: Date = new Date();
-            nextMonth.setMonth(nextMonth.getMonth() + 1);
-            const newItem:myusrinfo = {
-              MembershipEnd: nextMonth,
-              MembershipType: 'Demo',
-              projectLocation: '/projectList/DemoProjectKey',
-              projectOwner: false,
-              projectName: 'Demo'
-            };
-            this.myuserProfile.myusrinfoFromDb=newItem;
-            this.getProfileInfoSubscription?.unsubscribe();
-            this.getSectionsSubscription?.unsubscribe();
-            this.getPublicListSubscription?.unsubscribe();
-            this.getPrivateListSubscription?.unsubscribe();
+
     this.developmentservice.logout();
   }
 }
