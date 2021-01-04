ff7c61aed9cf712b2551828a7f7fbe8ad309bc98
gmanojisaac <gmanoj.isaac@gmail.com>
2021-01-04 09:56:49 +0530

    Working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -222,6 +222,7 @@ export class AppComponent {
   }
   componentLogOff() {
     this.getSectionsSubscription?.unsubscribe();
+    this.getTestcasesSubscription?.unsubscribe();
     this.developmentservice.logout();
   }
 }
